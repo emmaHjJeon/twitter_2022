eb869340a4eddf1f66a7be6858044a062e099fdb
emmaHjJeon <93476644+emmaHjJeon@users.noreply.github.com>
2022-10-02 19:45:02 +0900

    connect database

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
+import "firebase/compat/firestore"; 
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,3 +21,4 @@ firebase.initializeApp(firebaseConfig);
 
 export const firebaseInstance = firebase; 
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();  
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,64 @@
-const Home = () => <span> Home</span>; 
+import { useEffect, useState } from "react"; 
+import { dbService } from "fbase";
+
+const Home = ({userObj}) => {
+    console.log(userObj); 
+    const [tweet, setTweet] = useState(""); 
+    const [tweets, setTweets] = useState([]); 
+
+    useEffect(() => {
+        dbService.collection("tweets").onSnapshot((snapshot) => {
+            const newArray = snapshot.docs.map((document) => ({
+                id: document.id, 
+                ...document.data(), 
+            })); 
+            setTweet(newArray); 
+        }); 
+    }, []); 
+
+    const onSubmit = async (event) => {
+        event.preventDefault(); 
+
+        await dbService.collection("tweets").add({
+            text: tweet, 
+            createdAt: Date.now(), 
+            createdId: userObj.uid,
+        }); 
+
+        setTweet(""); 
+    }; 
+
+    const onChange = (event) => {
+        event.preventDefault(); 
+        
+        const {
+            target : { value }, 
+        } = event; 
+
+        setTweet(value); 
+    }
+
+    return (
+        <>
+            <form onSubmit={onSubmit}>
+                <input 
+                    value={tweet}
+                    onChange={onChange} 
+                    type="text"
+                    placeholder="What's on your mind?"
+                    maxLength={120}
+                /> 
+                <input type="submit" value="Tweet" /> 
+            </form>
+            <div> 
+                {tweets.map((tweet) => (
+                    <div key={tweet.id}> 
+                        <h4>{tweet.text}</h4> 
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+}; 
 
 export default Home; 
